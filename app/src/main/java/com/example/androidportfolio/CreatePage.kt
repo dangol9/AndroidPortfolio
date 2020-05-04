5a92fe0bea4383e87edc320df0e31ecf4c642493
Andreasmalleus <andreasmalleus@gmail.com>
2020-05-04 16:12:15 +0300

    file views added

--- app/src/main/java/com/example/androidportfolio/CreatePage.kt
+++ app/src/main/java/com/example/androidportfolio/CreatePage.kt
@@ -1,15 +1,11 @@
 package com.example.androidportfolio
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.os.StrictMode
-import android.os.StrictMode.VmPolicy
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.itextpdf.text.Chunk
 import com.itextpdf.text.Document
 import com.itextpdf.text.Paragraph
 import com.itextpdf.text.pdf.PdfWriter
@@ -19,47 +15,20 @@ import java.io.FileOutputStream
 
 class CreatePage : AppCompatActivity() {
 
-    private val STORAGE_CODE: Int = 10;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_page)
         bottomNavBarListenerSetup();
+        createPdfButtonListener();
 
+    }
+
+    private fun createPdfButtonListener(){
         addData.setOnClickListener {
-            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
-                if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                    //IF DENIED ASK
-                    val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                    requestPermissions(permissions, STORAGE_CODE)
-                }else{
-                    savePdf();
-                }
-            }else{
-                savePdf();
-            }
+            savePdf();
         }
     }
-
-    private fun bottomNavBarListenerSetup(){
-
-        bottom_navigation.setOnNavigationItemSelectedListener {item ->
-            when(item.itemId){//like switch statement
-                R.id.action_create -> {
-                    Toast.makeText(this, "Create", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.action_manage ->{
-                    Toast.makeText(this, "Manage", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, ManagePage::class.java);
-                    startActivity(intent);
-                    true
-                }
-                else->true
-            }
-        }
-    }
-
 
     private fun savePdf() {
         val mFileName = fileName.text.toString();
@@ -70,11 +39,7 @@ class CreatePage : AppCompatActivity() {
             //open for writing
             mDoc.open();
 
-            val name = surname.text.toString();
-            val famName = familyName.text.toString();
-
-            mDoc.addAuthor(name + " " + famName);
-            mDoc.add(Paragraph("Test" + name + " " + " on tema nimi ja" + famName));
+            createPdfTemplate(mDoc);
 
             mDoc.close()
             Log.d("Path is: ", mFilePath);
@@ -86,16 +51,31 @@ class CreatePage : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when(requestCode){
-            STORAGE_CODE ->  {
-                if(grantResults.size  >  0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    savePdf()
-                }else{
-                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
+    private fun createPdfTemplate(mDoc: Document){
+        val name = surname.text.toString();
+        val famName = familyName.text.toString();
+
+        mDoc.addAuthor(name + " " + famName);
+        mDoc.add(Paragraph("Test" + name + " " + " on tema nimi ja" + famName));
+        mDoc.add(Chunk.NEWLINE);
+    }
+
+    private fun bottomNavBarListenerSetup(){
+
+        bottom_navigation.setOnNavigationItemSelectedListener {item ->
+            when(item.itemId){//like switch statement
+                R.id.action_create -> {
+                    true
                 }
+                R.id.action_manage ->{
+                    val intent = Intent(this, ManagePage::class.java);
+                    startActivity(intent);
+                    true
+                }
+                else->true
             }
         }
     }
+
 
 }
--- app/src/main/java/com/example/androidportfolio/ManagePage.kt
+++ app/src/main/java/com/example/androidportfolio/ManagePage.kt
@@ -1,14 +1,15 @@
 package com.example.androidportfolio
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
 import android.util.Log
+import android.view.Gravity
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_manage_page.*
 import java.io.File
@@ -16,12 +17,16 @@ import java.io.File
 
 class ManagePage : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_page)
-        bottomNavBarListenerSetup();
-        listFilesInDirectory();
+        val path = getExternalFilesDir(null).toString() + "/";
 
+        bottomNavBarListenerSetup();
+        listFilesInDirectory(path);
+
+        //essential if sdk >= 24
         val builder = VmPolicy.Builder()
         StrictMode.setVmPolicy(builder.build())
         builder.detectFileUriExposure()
@@ -29,47 +34,75 @@ class ManagePage : AppCompatActivity() {
     }
 
     //hiljem proovida listina
-    private fun listFilesInDirectory(){
-        val path = getExternalFilesDir(null).toString() + "/";
+    private fun listFilesInDirectory(path: String){
         Log.d("Files", "Path: " + path);
         val files = File(path).listFiles()
         Log.d("Files", "Size: " + files.size)
         val intent = Intent(Intent.ACTION_VIEW);
         for (i in files.indices) {
             Log.d("Files", "FileName:" + files[i].name)
-            val fileContainer = TextView(this);
-            fileContainer.textSize = 30f;
-            fileContainer.text = files[i].name;
-            fileContainer.setOnClickListener(View.OnClickListener {
-                intent.setDataAndType(Uri.fromFile(File(path + files[i].name.toString())), "application/pdf")
-                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                startActivity(intent);
-            }
-            )
-            manageLayout.addView(fileContainer);
+            buildView(files[i], path);
 
         }
 
     }
 
+    private fun buildView(file: File, path: String){
+        val parentLayout = LinearLayout(this);
+        parentLayout.orientation = LinearLayout.HORIZONTAL;
+        parentLayout.gravity = Gravity.CENTER;
+        val textViewLayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+        textViewLayoutParams.setMargins(25, 20, 25, 10);
+
+        val fileNameView = TextView(this);
+        fileNameView.textSize = 20f;
+        fileNameView.text = file.name;
+        fileNameView.setOnClickListener(View.OnClickListener {
+            intent.setDataAndType(Uri.fromFile(File(path + file.name.toString())), "application/pdf")
+            startActivity(intent);
+        });
+
+        val fileEditIconView = TextView(this);
+        fileEditIconView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_edit, 0,0,0);
+        fileEditIconView.setTextColor(Color.BLACK);
+        fileEditIconView.setOnClickListener{
+            Toast.makeText(this,"Edit", Toast.LENGTH_SHORT).show();
+        }
+
+        val fileRemoveIconView = TextView(this);
+        fileRemoveIconView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_remove, 0,0,0);
+        fileRemoveIconView.setTextColor(Color.BLACK);
+        fileRemoveIconView.setOnClickListener{
+            Toast.makeText(this,"Remove", Toast.LENGTH_SHORT).show();
+            file.delete();
+            finish();
+            startActivity(getIntent());
+        }
+
+        parentLayout.addView(fileNameView, textViewLayoutParams)
+        parentLayout.addView(fileEditIconView, textViewLayoutParams);
+        parentLayout.addView(fileRemoveIconView, textViewLayoutParams);
+        manageLayout.addView(parentLayout);
+    }
+
 
     private fun bottomNavBarListenerSetup(){
-
         bottom_navigation.setOnNavigationItemSelectedListener {item ->
             when(item.itemId){//like switch statement
                 R.id.action_create -> {
-                    Toast.makeText(this, "Create", Toast.LENGTH_SHORT).show()
                     val intent = Intent(this, CreatePage::class.java);
                     startActivity(intent);
                     true
                 }
                 R.id.action_manage ->{
-                    Toast.makeText(this, "Manage", Toast.LENGTH_SHORT).show()
                     true
                 }
                 else->true
             }
         }
     }
+
 }
 
